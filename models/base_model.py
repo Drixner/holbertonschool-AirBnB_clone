0f0db3796d7808c1c3a1a7140c643310688d24c4
Drixner <drixnerrafael1@gmail.com>
2022-10-12 22:06:21 -0500

    Write `BaseModel` that defines attributes/methods for other classes

--- models/base_model.py
+++ models/base_model.py
@@ -22,6 +22,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.today()
             self.updated_at = datetime.today()
+            models.storage.new(self)
 
     def __str__(self):
         """ Type method __str__ """
@@ -31,6 +32,7 @@ class BaseModel:
     def save(self):
         """ Type method save """
         self.updated_at = datetime.today()
+        models.storage.save(self)
 
     def to_dict(self):
         """ Type method to_dict """
