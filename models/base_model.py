d7a017ee7a5a393c2d1e665f9440a9dc6e16456a
Salomon Chambi <salomonchambi@gmail.com>
2022-10-12 19:50:17 -0700

    Write BaseModel that defines attributes/methods for other classes

--- models/base_model.py
+++ models/base_model.py
@@ -1,38 +1,43 @@
 #!/usr/bin/python3
-"""
-Module BaseModel
-"""
+"""Type module of BaseModel"""
+
 import models
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """class BaseModel"""
+    """Type class of BaseModel"""
 
-    def __init__(self):
-        """ constructor of BaseModel """
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.strptime('%Y-%m-%dT%H:%M:%S.%f')
-        self.updated_at = datetime.strptime('%Y-%m-%dT%H:%M:%S.%f')
+    def __init__(self, *args, **kwargs):
+        """Type method initialize"""
+        timeformat = "%Y-%m-%dT%H:%M:%S.%f"
+        if len(kwargs) != 0:
+            for key, val in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(val, timeformat))
+                elif key != '__class__':
+                    setattr(self, key, val)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
+            models.storage.new(self)
 
     def __str__(self):
-        """ method string representation """
+        """Type method __str__"""
         class_name = self.__class__.__name__
         return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
     def save(self):
-        """  current datetime object """
-
-        self.updated_at = datetime.now()
+        """Type method save"""
+        self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
-        """ dictionary representation """
-
-        dict_cp = self.__dict__.copy()
-        dict_cp["created_at"] = self.created_at.isoformat()
-        dict_cp["updated_at"] = self.updated_at.isoformat()
-        dict_cp["__class__"] = self.__class__.__name__
-
-        return dict_cp
+        """Type method to_dict"""
+        rt_dict = self.__dict__.copy()
+        rt_dict["created_at"] = self.created_at.isoformat()
+        rt_dict["updated_at"] = self.updated_at.isoformat()
+        rt_dict["__class__"] = self.__class__.__name__
+        return rt_dict
