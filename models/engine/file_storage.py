775ac1f46503b847db3e4e174531d9ac972597f1
Salomon Chambi <salomonchambi@gmail.com>
2022-10-14 21:09:14 -0700

    Write `FileStorage` that serialize and deserialize a file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,17 +7,33 @@ import json
 import os.path
 
 
-
 class FileStorage:
-    """ class File Storgae """
+    """ class File Storage serialize and deserialize JSON objects """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """ public instance method all """
+        """ Return the dictionary __objects """
         return FileStorage.__objects
 
     def new(self, obj):
-        """ public instance method new """
+        """ Set in __object the obj with the key <obj class name>.id """
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__objects[key] = obj
+
+    def save(self):
+        """ Serialize __objects to JSON file """
+        dictionary = {}
+        for key, value in FileStorage.__objects.items():
+            dictionary[key] = value.to_dict()
+
+        with open(FileStorage.__file_path, 'w') as fd:
+            json.dump(dictionary, fd)
+
+    def reload(self):
+        """ Deserialize __objects from JSON file """
+        if os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path) as fd:
+                obj_dict = json.load(fd)
+
+            return
