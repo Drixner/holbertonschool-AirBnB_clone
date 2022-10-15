92d2ec2896133aaa1faf271244849e5498c754f3
Drixner <drixnerrafael1@gmail.com>
2022-10-15 10:40:33 -0500

    Contain the entry point of the command interpreter

---
+++ console.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Console to manage hbnb data """
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """ Class HBNBCommand CLI, entry command interpreter """
+    prompt = '(hbnb) '
+
+    def emptyline(self):
+        """ Method emptyline """
+        pass
+
+    def do_quit(self, line):
+        """ Exit the shell """
+        return True
+
+    def do_EOF(self, line):
+        """ Exit the shell """
+        print()
+        retur True
