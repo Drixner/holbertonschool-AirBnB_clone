f45b684bb847671b6778865dcdebf02906b6e703
Salomon Chambi <salomonchambi@gmail.com>
2022-10-16 13:32:08 -0700

    Write `State` class that inherits from `BaseModel`

---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module class state """
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ Class State that inherits from BaseModel """
+    name = ""
