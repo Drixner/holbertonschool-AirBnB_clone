4199b7781e960456598b700a468255a4f603ea76
Salomon Chambi <salomonchambi@gmail.com>
2022-10-15 21:24:34 -0700

    Update `console.py` add create, show, destroy, all and update commands

--- console.py
+++ console.py
@@ -92,6 +92,35 @@ class HBNBCommand(cmd.Cmd):
         if yes == 0:
             print("** class doesn't exist **")
 
+    def do_update(self, line):
+        """Update if given exact object, exact attribute"""
+        args = parse(line)
+        if len(args) >= 4:
+            key = "{}.{}".format(args[0], args[1])
+            cast = type(eval(args[3]))
+            arg3 = args[3]
+            arg3 = arg3.strip('"')
+            arg3 = arg3.strip("'")
+            setattr(storage.all()[key], args[2], cast(arg3))
+            storage.all()[key].save()
+        elif len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif ("{}.{}".format(args[0], args[1])) not in storage.all().keys():
+            print("** no instance found **")
+        elif len(args) == 2:
+            print("** attribute name missing **")
+        else:
+            print("** value missing **")
+
+
+def parse(line):
+    """Helper method to parse user typed input"""
+    return tuple(line.split())
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
