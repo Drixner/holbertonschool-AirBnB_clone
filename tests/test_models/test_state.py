8bb33f6c3db4c91c8271ba8089c92113292edc3b
Salomon Chambi <salomonchambi@gmail.com>
2022-10-16 22:00:56 -0700

    Add unit test for State

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,51 +1,37 @@
 #!/usr/bin/python3
-"""
-Unittest for state module
-"""
-import os
-import unittest
-from models.state import State
+"""Test State"""
+from models.amenity import Amenity
 from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+import pep8
+import unittest
 
 
-class Test_State(unittest.TestCase):
-    """ Test for
-    State Class """
+class Teststate(unittest.TestCase):
+    """
+    Unittests for the State class.
+    """
 
-    def setUp(self):
-        """set up the
-        test for testing States"""
-        FileStorage._FileStorage__file_path = "test.json"
-        self.state = State()
-        self.state.name = "Arequipa"
-        self.state.save()
+    def test_pep8_conformance_state(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/state.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def test_docstring_State(self):
-        """checking for docstrings"""
-        self.assertIsNotNone(State.__doc__)
+    def test_class(self):
+        """
+        Tests if class is named correctly.
+        """
+        state1 = State()
+        self.assertEqual(state1.__class__.__name__, "State")
 
-    def test_instance_State(self):
-        """checking for valid type"""
-        self.assertTrue(type(self.state.name) is str)
-
-    def test_to_dict_State(self):
-        """test if dictionary works"""
-        self.assertEqual('to_dict' in dir(self.state), True)
-
-    def testpublic(self):
-        self.assertEqual(str, type(State().id))
-
-    def testHasAttributes(self):
-        """verify if attributes exist"""
-        self.assertTrue(hasattr(self.state, 'name'))
-        self.assertTrue(hasattr(self.state, 'id'))
-        self.assertTrue(hasattr(self.state, 'created_at'))
-        self.assertTrue(hasattr(self.state, 'updated_at'))
-
-    def tearDown(self):
-        os.remove(FileStorage._FileStorage__file_path)
-
-
-if __name__ == "__main__":
-    unittest.main()
+    def test_father(self):
+        """
+        Tests if Class inherits from BaseModel.
+        """
+        state1 = State()
+        self.assertEqual(state1.__class__.__name__, "State")
